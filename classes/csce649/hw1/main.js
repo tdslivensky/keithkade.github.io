7649e8d58121b2ecc3b7f33848707f159e40f865
keithkade <keithkade@aol.com>
2015-09-15 01:26:51 -0500

    added rest detection and basic ui for modifying inputs

--- classes/csce649/hw1/main.js
+++ classes/csce649/hw1/main.js
@@ -12,8 +12,8 @@
 
 var doc = document; //shorthand
 
-var SCENE_WIDTH = 650;
-var SCENE_HEIGHT = 650;
+var SCENE_WIDTH = 620;
+var SCENE_HEIGHT = 620;
 var CUBE_LEN = 10;
 
 var VIEW_ANGLE = 45;
@@ -56,10 +56,10 @@ var planes = [
 /** create the renderer and add it to the scene */
 function initRenderer(){
     var renderer = new THREE.WebGLRenderer({ alpha: true });
-    renderer.setClearColor(0x888888 , 1); //make the background grey
+    renderer.setClearColor(0xD4D4D4 , 1); //make the background grey
 
     renderer.setSize(SCENE_WIDTH, SCENE_HEIGHT);
-    doc.getElementById('container').appendChild(renderer.domElement);
+    doc.getElementById('webgl-container').appendChild(renderer.domElement);
     return renderer;
 }
 
@@ -84,10 +84,10 @@ function initLight(){
     return pointLight;
 }
 
-/** create the box and add it to the scene 
+/** 
+ *  create the box and add it to the scene 
  *  What happens here is that I create both the box (mesh) and a box helper that 
- *  is the bounding box of that mesh. I set the original box to not be visible,
- *  leacing just the edges
+ *  is the bounding box of that mesh. I set the original box to not be visible, leaving just the edges
  */
 function initCube(){
     var geometry = new THREE.BoxGeometry(CUBE_LEN, CUBE_LEN, CUBE_LEN);
@@ -110,22 +110,41 @@ var camera = initCamera();
 var light = initLight();
 var cube = initCube();
 
-
-
 /************* Assignment specific code begins here *************/
 
 //variables that the user will be able to adjust
-var G = new $V([0, -10, 0]); //the accel due to gravity in m/s^2 -9.81
-var D = 0.4;
-var H = 0.01; // in seconds. Step time
-var H_MILLI = H * 1000; 
-var CR = 0.5;
-var CF = 0.5;
+var G = new $V([0, -9.81, 0]);  // The accel due to gravity in m/s^2 
+var D = 0.4;                    // Air resitence
+var H = 0.01;                   // Step time in seconds
+var H_MILLI = H * 1000;         // In milliseconds
+var CR = 0.5;                   // coefficient of restitution
+var CF = 0.5;                   // coefficient of TODO
+
+var minCollisionV = 1;          // TODO
+var minCollisionX = 1;          // TODO
+
+var initialX = {x: CUBE_LEN/2, y: CUBE_LEN/2, z: CUBE_LEN/2};
+var initialV = {x: 60, y: 16, z: -21};
+
+function getUserInputs(){
+    initialX.x = parseInt(doc.getElementById("p.x").value);
+    initialX.y = parseInt(doc.getElementById("p.y").value);
+    initialX.z = parseInt(doc.getElementById("p.z").value);
+
+    initialV.x = parseInt(doc.getElementById("v.x").value);
+    initialV.y = parseInt(doc.getElementById("v.y").value);
+    initialV.z = parseInt(doc.getElementById("v.z").value);    
+}
 
 var sphere, clock;
 function initMotion(){
-    sphere = new Sphere(scene, [CUBE_LEN/2, CUBE_LEN/2, CUBE_LEN/2], [60, 16, 0], RADIUS);
-    //sphere = new Sphere(scene, [0, 100, 0], [10,0,30]);
+    getUserInputs();
+    if (sphere){
+        scene.remove(sphere.visual);
+    }
+    
+    //TODO breaks on [60, 16, -20]. not sure if it a rounding error or what
+    sphere = new Sphere(scene, [initialX.x, initialX.y, initialX.z], [initialV.x, initialV.y, initialV.z], RADIUS);
     clock = new THREE.Clock();
     clock.start();
     clock.getDelta();
@@ -139,9 +158,6 @@ function integrate(v1, v2, timestep){
 }
 
 function simulate(){
-    
-    //TODO Collision Determination
-    //TODO Collision Response
     
     var timestepRemain = H;
     var timestep = timestepRemain; // We try to simulate a full timestep 
@@ -159,6 +175,12 @@ function simulate(){
         
         var collision = collisionFraction(sphere.x, xNew);
         if (collision){
+
+            //If the ball is at rest stop simulation
+            if (atRest(vNew, xNew, acceleration, collision)){
+                return;    
+            }
+            
             timestep = collision.fraction * timestep;
             vNew = integrate(sphere.v, acceleration, timestep);            
             
@@ -172,9 +194,7 @@ function simulate(){
         sphere.x = xNew;
         sphere.visual.position.set(xNew.elements[0], xNew.elements[1], xNew.elements[2]);
     }
-    
-    //TODO AT REST DETECTION
-    
+        
     var waitTime = H_MILLI - clock.getDelta(); 
 
     //4 milliseconds is the minimum wait 
@@ -185,18 +205,20 @@ function simulate(){
     setTimeout(simulate, waitTime);
 }
 
-//smartly render
+/** rendering loop */
 function render() {	
     renderer.render(scene, camera); //draw it
-	requestAnimationFrame(render);  //let the browser decide the best time to redraw
+	requestAnimationFrame(render);  //redraw whenever the browser refreshes
 }
  
-function collisionFraction(v1, v2){
+
+/** Detects at what fraction a collision occurs. Returns false if there is no collision. */
+function collisionFraction(x1, x2){
     
     for (var i = 0; i < planes.length; i++){
         var plane = planes[i];
-        var dOld = v1.subtract(plane.p).dot(plane.n);
-        var dNew = v2.subtract(plane.p).dot(plane.n);
+        var dOld = x1.subtract(plane.p).dot(plane.n);
+        var dNew = x2.subtract(plane.p).dot(plane.n);
                     
         //check if they have the same sign
         //if dOld is zero then a collision just happened and we don't want to detect it again
@@ -206,7 +228,8 @@ function collisionFraction(v1, v2){
         else {
             return { 
                 fraction: dOld / (dOld-dNew),
-                normal: plane.n
+                normal: plane.n,
+                point: plane.p
             };
         }
     }
@@ -224,3 +247,29 @@ function collisionResponse(vOld, n){
     
     return vNormalNew.add(vTanNew);
 }
+
+/**
+ * Criteria for it being at rest.
+ * 1. velocity less than minCollisionV
+ * 2. dist less than minCollisionX
+ * 3. acceleration is away from the surface
+ */
+function atRest(v, x, a, collision){
+    var dist = x.subtract(collision.point).dot(collision.normal);
+    if (magnitude(v) < minCollisionV && dist < minCollisionX && a.dot(collision.normal) < 0){
+        return true;
+    }
+    return false;
+}
+
+/**
+ *  Strangely, Sylvester doesn't have magnitude. That or I am blind and couldn't find it in the API docs.
+ */
+function magnitude(v){
+    var sum = 0;
+    for (var i = 0; i < v.elements.length; i++){
+        sum += Math.pow(v.elements[i], 2);
+    }
+    return Math.sqrt(sum);
+}
+
