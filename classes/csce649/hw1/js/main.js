f7390ac5069d0ed7b7b7be2c268bacb843bfb4c8
keithkade <keithkade@aol.com>
2015-09-17 10:09:22 -0500

    made more settings adjustable

--- classes/csce649/hw1/js/main.js
+++ classes/csce649/hw1/js/main.js
@@ -1,7 +1,8 @@
-/*global document, THREE, setInterval, setTimeout, requestAnimationFrame, waitTime, console, $V, Sphere*/ 
+/*global document, THREE, setInterval, setTimeout, requestAnimationFrame, waitTime, console, $V, Sphere, window*/ 
 
 /** 
  *   I am using Three.js, a webgl library: http://threejs.org/
+ *   I am using a Three.js orbital control library to let the user control the camera with the mouse
  *   I used this as my starting code: https://aerotwist.com/tutorials/getting-started-with-three-js/
  *   That contained the basics of setting up a scene, camera, light, and object
  * 
@@ -12,19 +13,28 @@
 
 var doc = document; //shorthand
 
-var SCENE_WIDTH = 620;
-var SCENE_HEIGHT = 620;
-var CUBE_LEN = 10;
+var SCENE_WIDTH = window.innerWidth - 430; //430 is width of options panel
+var SCENE_HEIGHT = window.innerHeight - 5; //Three js makes the canvas a few pixels too big so the minus five fixes that 
+var CUBE_LEN = 20; //unless otherwise noted, measurements are usually in meters
 
-var VIEW_ANGLE = 45;
+var FIELD_OF_VIEW = 45;
 var ASPECT = SCENE_WIDTH / SCENE_HEIGHT;
 var NEAR = 0.1;
 var FAR = 10000;
 
-var RADIUS = 1; //radius of sphere in meters
+var RADIUS = 2; //radius of sphere in meters
+
+var scene = new THREE.Scene();
+var renderer = initRenderer();
+var camera = initCamera();
+var light = initLight();
+var cube = initCube();
+var axes = initAxes();
+var sphere, clock;
+var isSimulating;   //is the simulation currently running?
 
 //we have six planes that we check for collision, defined by a point and the normal. 
-//this data structure could be optimized
+//this data structure could probably be optimized
 var planes = [
     {
         p: $V([0,RADIUS,0]), //bottom
@@ -45,7 +55,6 @@ var planes = [
     {
         p: $V([RADIUS,0,0]), //left
         n: $V([1,0,0]),
-        id: 'temp'
     }, 
     {
         p: $V([CUBE_LEN-RADIUS,0,0]), //right
@@ -64,14 +73,17 @@ function initRenderer(){
 }
 
 /** create the camera and add it to the scene */
-function initCamera(){
-    var camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
+function initCamera(){    
+    var camera = new THREE.PerspectiveCamera( FIELD_OF_VIEW, ASPECT, NEAR, FAR);
 
-    camera.position.set(CUBE_LEN*2.1, CUBE_LEN*2.1, CUBE_LEN*2.6);
+    camera.position.set(CUBE_LEN*2, CUBE_LEN*2, CUBE_LEN*3);
 
-    camera.lookAt(new THREE.Vector3(0,0,0)); //we want to focus on the center always
+    camera.lookAt(new THREE.Vector3(0,0,0)); //we want to focus on the center 
     
     scene.add(camera);
+    
+    var controls = new THREE.OrbitControls(camera, renderer.domElement);
+
     return camera;
 }
     
@@ -97,34 +109,93 @@ function initCube(){
     mesh.position.set(CUBE_LEN/2, CUBE_LEN/2, CUBE_LEN/2);
 
     var box = new THREE.BoxHelper( mesh );
-    box.material.color.set( 0xff0000 );
+    box.material.color.set( 0xCC3300 );
     
     scene.add(mesh);
     scene.add(box);
     return box;
 }
 
-var scene = new THREE.Scene();
-var renderer = initRenderer();
-var camera = initCamera();
-var light = initLight();
-var cube = initCube();
+/** draw x, y and z axes */
+function initAxes(){
+    var length = CUBE_LEN * 3;
+    var axes = new THREE.Object3D();
+    
+    //lines
+    axes.add(initLine(new THREE.Vector3(-length, 0, 0), new THREE.Vector3(length, 0, 0), 0xff0000)); // X 
+    axes.add(initLine(new THREE.Vector3(0, -length, 0), new THREE.Vector3(0, length, 0), 0x00ff00)); // Y
+    axes.add(initLine(new THREE.Vector3(0, 0, -length), new THREE.Vector3(0, 0, length), 0x0000ff)); // Z
+    
+    //labels
+    axes.add(initLabel('X','#ff0000', [CUBE_LEN + 5, 0, 0]));
+    axes.add(initLabel('Y','#00ff00', [0, CUBE_LEN + 5, 0]));
+    axes.add(initLabel('Z','#0000ff', [0, 0, CUBE_LEN + 5]));
+    
+    //scene.add(axes);
+    return axes;
+}
+
+/** Create a line that goes between the two points of the given color*/
+function initLine(v1, v2, col){
+    var material = new THREE.LineBasicMaterial({ color: col });
+    var geometry = new THREE.Geometry();
+    geometry.vertices.push(v1);
+    geometry.vertices.push(v2);
+    var line = new THREE.Line(geometry, material);
+    return line;
+}
+
+function toggleAxes(elem){
+    if (elem.checked){
+        scene.add(axes);
+    }
+    else {
+        scene.remove(axes);
+    }
+}
+
+/** Creates a canvas with the given text then renders that as a sprite. Original: http://stackoverflow.com/questions/14103986/canvas-and-spritematerial */
+function initLabel(text, color, coords){
+
+    var canvas = doc.createElement('canvas');
+    var size = 300;
+    canvas.width = size;
+    canvas.height = size;
+    
+    var context = canvas.getContext('2d');
+    context.textAlign = 'center';
+    context.fillStyle = color;
+    context.font = '90px Helvetica';
+    context.fillText(text, size/2, size/2);
+
+    var amap = new THREE.Texture(canvas);
+    amap.needsUpdate = true;
+
+    var mat = new THREE.SpriteMaterial({
+        map: amap,
+        color: 0xffffff     
+    });
+
+    var sprite = new THREE.Sprite(mat);
+    sprite.scale.set( 10, 10, 1 ); 
+    sprite.position.set(coords[0], coords[1], coords[2]);
+    return sprite;  
+    
+}
 
 /************* Assignment specific code begins here *************/
 
 var G = new $V([0, -9.81, 0]);  // The accel due to gravity in m/s^2 
-var minRestV = 0.5;          // minimum velocity to declare at rest
+var minRestV = 0.1;          // minimum velocity to declare at rest
 
-//variables that the user will be able to adjust
-var H;                  // Step time in seconds
-var H_MILLI;            // In milliseconds
-var initialX = {};
-var initialV = {};
-
-var D;            // Air resitence
-
-var CR = 0.5;           // coefficient of restitution. 1 is maximum bouncy
-var CF = 0.2;           // coefficient of friction. 0 is no friction
+//variables that the user sets
+var H;              // Step time in seconds
+var H_MILLI;        // In milliseconds
+var initialX = {};  // Position
+var initialV = {};  // Velocity
+var D;              // Air resitence
+var CR;             // coefficient of restitution. 1 is maximum bouncy
+var CF;             // coefficient of friction. 0 is no friction
 
 
 function getUserInputs(){
@@ -134,7 +205,11 @@ function getUserInputs(){
 
     initialV.x = parseFloat(doc.getElementById("v.x").value);
     initialV.y = parseFloat(doc.getElementById("v.y").value);
-    initialV.z = parseFloat(doc.getElementById("v.z").value);    
+    initialV.z = parseFloat(doc.getElementById("v.z").value); 
+    
+    G.elements[0] = parseFloat(doc.getElementById("g.x").value);
+    G.elements[1] = parseFloat(doc.getElementById("g.y").value);
+    G.elements[2] = parseFloat(doc.getElementById("g.z").value); 
 
     H = parseFloat(doc.getElementById("H").value);    
     H_MILLI = H * 1000;    
@@ -144,12 +219,23 @@ function getUserInputs(){
     CF = parseFloat(doc.getElementById("CF").value);    
 }
 
-/** update one input value with another. this binds the sliders to the text inputs */
-function update(elem, id){
-    doc.getElementById(id).value = elem.value;
+/** 
+ * refresh the values of our constants on input change. and tell simulation to start back up if neccesary
+ * also binds the sliders to the text inputs using the optional elem and id parameters
+ */
+function inputChange(elem, id){
+    if (elem && id){
+        doc.getElementById(id).value = elem.value;
+    }
+    getUserInputs();
+    
+    if (!isSimulating){
+        isSimulating = true;
+        simulate();
+    }
 }
 
-var sphere, clock;
+/** create the sphere and set it according to user inputs, then start simulation and rendering */
 function initMotion(){
     getUserInputs();
     if (sphere){
@@ -160,15 +246,18 @@ function initMotion(){
     clock = new THREE.Clock();
     clock.start();
     clock.getDelta();
+    isSimulating = true;
     simulate();
     render();
 }
 initMotion();
 
+/** Euler integration */
 function integrate(v1, v2, timestep){
     return v1.add(v2.multiply(timestep));
 }
 
+/** the main simulation loop. recursive */ 
 function simulate(){
     
     var timestepRemain = H;
@@ -183,14 +272,12 @@ function simulate(){
         var vNew = integrate(sphere.v, acceleration, timestep);
         var xNew = integrate(sphere.x, sphere.v, timestep);
         
-        //old just gravity
-        //var vNew = sphere.v.add(G.multiply(H));
-        
         var collision = collisionFraction(sphere.x, xNew);
         if (collision){
 
             //If the ball is at rest stop simulation
             if (atRest(vNew, acceleration, collision)){
+                isSimulating = false;
                 return;    
             }
             
@@ -252,7 +339,7 @@ function collisionFraction(x1, x2){
         }
     }
     
-    //return the first collision with the minimum f
+    //return the collision with the minimum f
     if (collisions.length > 0){
         var minF = 1;
         var minIndex = 0;
@@ -275,8 +362,11 @@ function collisionResponse(vOld, n){
     var vTanOld = vOld.subtract(vNormalOld);
 
     var vNormalNew = n.multiply(-1 * CR * (vOld.dot(n)));
-    //TODO use the coulomb 
+     
     var vTanNew = vOld.subtract(vNormalOld).multiply(1-CF);
+    
+    //coulomb model. odd results.
+    //var vTanNew = vTanOld.subtract(vTanOld.toUnitVector().multiply(Math.max((1-CF)*magnitude(vNormalOld), magnitude(vTanOld))));
     
     return vNormalNew.add(vTanNew);
 }
@@ -295,7 +385,7 @@ function atRest(v, a, collision){
 }
 
 /**
- *  Strangely, Sylvester doesn't have magnitude. That or I am blind and couldn't find it in the API docs.
+ *  Strangely, Sylvester doesn't have a magnitude function 
  */
 function magnitude(v){
     var sum = 0;
